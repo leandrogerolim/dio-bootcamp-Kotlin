a5863905e94de30f7844bed0016bb39de974efd5
Leandro <leandrogerolim@gmail.com>
2022-12-06 16:31:36 -0400

    atividade de Expressão Condicional.

---
+++ EstruturaControleFluxoColecoesKotlin/src/main/kotlin/expressaoCondicional.kt
@@ -0,0 +1,8 @@
+
+    fun main() {
+        fun max(a: Int, b: Int) = if (a > b) a else b                     // 1 cria as duas variaveis A e B começa o
+                                                                            // if se a é maior que B imprima o valor de a
+                    //cria a função MAX como se tivesse um construtor inserido.    /// se não imprima o valor de b
+
+        println(max(99, 1442))
+    }
