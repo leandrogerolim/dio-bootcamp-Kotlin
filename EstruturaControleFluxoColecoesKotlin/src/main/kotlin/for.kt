4b56d4bfaf18698ca204a51786d3f90f8669f009
Leandro <leandrogerolim@gmail.com>
2022-12-06 16:03:53 -0400

    repositório criado atividade de For

--- EstruturaControleFluxoColecoesKotlin/src/main/kotlin/for.kt
+++ EstruturaControleFluxoColecoesKotlin/src/main/kotlin/for.kt
@@ -1,9 +1,9 @@
 fun main() {
 
-    val cakes = listOf("carrot", "cheese", "chocolate")
+    val cakes = listOf("carrot", "cheese", "chocolate", "Vanilla")
 
-    for (cake in cakes) {                               // 1
-        println("Yummy, it's a $cake cake!")
+    for (cake in cakes) {                               // 1 fazer um for dentro da lista cakes e chamar de cake cada elemento.
+        println("Yummy, it's a $cake cake!")        // sem repetir e sem ter que informar quantas vezes precisa repetir
     }
 
 }
