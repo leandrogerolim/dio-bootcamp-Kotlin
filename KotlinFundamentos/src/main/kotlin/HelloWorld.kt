4f08b9f44d8fc2e45b0e33ac79c56569b313303c
Leandro <leandrogerolim@gmail.com>
2022-12-06 11:20:21 -0400

    Atividade Kotlin bootcamp DIO Hello World.

---
+++ KotlinFundamentos/src/main/kotlin/HelloWorld.kt
@@ -0,0 +1,9 @@
+
+    fun main(args: Array<String>) {  //fun função.
+        //função main pode ficar vazia  "fun main(){"
+
+    println("Hello World!")  //não precisa do ";" no fim da linha.
+    println("Olá, Mundo!!!")  //não precisa do ";" no fim da linha.
+
+    println("Program arguments: ${args.joinToString()}")
+}
