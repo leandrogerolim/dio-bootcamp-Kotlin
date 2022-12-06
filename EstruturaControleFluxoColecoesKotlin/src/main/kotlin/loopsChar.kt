ba74bd0f70db4378b9511d2cf5d5d9b8db5b6282
Leandro <leandrogerolim@gmail.com>
2022-12-06 16:21:50 -0400

    atividade de Loops Int.

---
+++ EstruturaControleFluxoColecoesKotlin/src/main/kotlin/loopsChar.kt
@@ -0,0 +1,13 @@
+
+    fun main() {
+        for (c in 'a'..'d') {        // 1 aumenta de 1 em 1 incluindo o primeiro e o ultimo elemento
+            print(c)
+        }
+        print(" ")
+
+        for (c in 'z' downTo 's' step 2) { // 2 imprime do ultimo para o prmeiro pulando de dois em dois .
+            print(c)
+        }
+        print(" ")
+
+    }
