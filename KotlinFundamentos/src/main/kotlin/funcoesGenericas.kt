3860e309f3689d10dd01b00b90ea3e78ddde7ea7
Leandro <leandrogerolim@gmail.com>
2022-12-06 14:24:43 -0400

    Funções Genéricas Kotlin bootcamp DIO  atividades.

---
+++ KotlinFundamentos/src/main/kotlin/funcoesGenericas.kt
@@ -0,0 +1,26 @@
+class funcoesGenericas {
+
+    class MutableStack<E>(vararg items: E) {              // 1 cria a lista de elementos "E"
+
+        private val elements = items.toMutableList()
+
+        fun push(element: E) = elements.add(element)        // 2 adiciona os elementos
+
+        fun peek(): E = elements.last()                     // 3
+
+        fun pop(): E = elements.removeAt(elements.size - 1)
+
+        fun isEmpty() = elements.isEmpty()
+
+        fun size() = elements.size
+
+        override fun toString() = "MutableStack(${elements.joinToString()})"
+    }
+
+    fun <E> mutableStackOf(vararg elements: E) = MutableStack(*elements) //
+
+    fun main() {
+        val stack = mutableStackOf(0.62, 3.14, 2.7)
+        println(stack)
+    }
+}
