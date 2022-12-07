5d5c1e5a04ed75009fc4a8c17da286c63996eb6f
Leandro <leandrogerolim@gmail.com>
2022-12-07 18:01:09 -0400

    atividade de coleções listas (lists).

--- EstruturaControleFluxoColecoesKotlin/src/main/kotlin/Colecoes/listas.kt
+++ EstruturaControleFluxoColecoesKotlin/src/main/kotlin/Colecoes/listas.kt
@@ -2,22 +2,25 @@ package Colecoes
 
 
 
-    val systemUsers: MutableList<Int> = mutableListOf(1, 2, 3)        // 1
-    val sudoers: List<Int> = systemUsers                              // 2
+    val systemUsers: MutableList<Int> = mutableListOf(1, 2, 3)        // variável não é multavél a  lista é  mutavél .
+    val sudoers: List<Int> = systemUsers                              //  a sgunda variável recebe uma copia somente leitrua de systemUsers somente consulta.
 
-    fun addSystemUser(newUser: Int) {                                 // 3
+    fun addSystemUser(newUser: Int) {                                 // 3 recebe um novo usuario e adiciona na lista.
         systemUsers.add(newUser)
     }
 
-    fun getSysSudoers(): List<Int> {                                  // 4
+    fun getSysSudoers(): List<Int> {                                  // 4 retorna a variavel de leitura
         return sudoers
     }
 
     fun main() {
-        addSystemUser(4)                                              // 5
-        println("Tot sudoers: ${getSysSudoers().size}")               // 6
+        addSystemUser(6)                                              // 5 adiciona 4 usuario.
+        println("Tot sudoers: ${getSysSudoers().size}")               // 6 imprime o tamanho da lista
         getSysSudoers().forEach {                                     // 7
                 i -> println("Some useful info on user $i")
+
+
         }
-        // getSysSudoers().add(5) <- Error!                           // 8
+        //getSysSudoers().add(5)
+                               // 8
     }
