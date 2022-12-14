e9c64b6fc3246fe44da0b21fd704dd16e5e04371
Leandro <leandrogerolim@gmail.com>
2022-12-14 19:56:24 -0400

    Orientação a Objetos e Tipos de Classes na Prática com Kotlin

--- Orientação a Objetos e Tipos de Classes na Prática com Kotlin/POO/src/main/POO/PooExemplos.kt
+++ Orientação a Objetos e Tipos de Classes na Prática com Kotlin/POO/src/main/POO/PooExemplos.kt
@@ -1,2 +1,13 @@
-class PooExemplos {
-}
+//1 - Herança Simples
+
+//2 - Herança Com Construtor Parametrizado
+
+//3 - Herança Passando Argumentos do Construtor para a Superclasse
+
+//4 - Data Classes
+
+//5 - Enum Classes
+
+//6 - Sealed Classes
+
+//7 - Object Keyword
