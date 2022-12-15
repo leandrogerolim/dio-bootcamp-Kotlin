531a12aea6e30cc6eca4c2d437c7e4cf408e358f
Leandro <leandrogerolim@gmail.com>
2022-12-15 12:56:35 -0400

    O Poder das Funções de Escopo em Kotlin.

--- O Poder das Funções em Kotlin/FuncoesKotlin/src/main/kotlin/FuncoesEscopo/Let.kt
+++ O Poder das Funções em Kotlin/FuncoesKotlin/src/main/kotlin/FuncoesEscopo/Let.kt
@@ -1,7 +1,38 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun customPrint(s: String) {
+    print(s.uppercase())
+}
+
+fun main() {
+
+    val empty = "test".let {               // 1
+        customPrint(it)                    // 2
+        it.isEmpty()                       // 3
+    }
+    println(" is empty: $empty")
+
+
+    fun printNonNull(str: String?) {
+        println("Printing \"$str\":")
+
+        str?.let {                         // 4
+            print("\t")
+            customPrint(it)
+            println()
+        }
+    }
+
+    fun printIfBothNonNull(strOne: String?, strTwo: String?) {
+        strOne?.let { firstString ->       // 5
+            strTwo?.let { secondString ->
+                customPrint("$firstString : $secondString")
+                println()
+            }
+        }
+    }
+
+    printNonNull(null)
+    printNonNull("my string")
+    printIfBothNonNull("First","Second")
+
 }
