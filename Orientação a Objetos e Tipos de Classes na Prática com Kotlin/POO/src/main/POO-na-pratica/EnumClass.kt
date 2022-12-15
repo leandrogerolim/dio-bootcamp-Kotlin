539d7f7dd122c8f325801bb76f39a7842908a103
Leandro <leandrogerolim@gmail.com>
2022-12-14 23:34:22 -0400

    Orientação a Objetos e Tipos de Classes na Prática com Kotlin

--- Orientação a Objetos e Tipos de Classes na Prática com Kotlin/POO/src/main/POO-na-pratica/EnumClass.kt
+++ Orientação a Objetos e Tipos de Classes na Prática com Kotlin/POO/src/main/POO-na-pratica/EnumClass.kt
@@ -1,10 +1,10 @@
 class EnumClass {
 }enum class State {
-    IDLE, RUNNING, FINISHED                           // 1
+    IDLE, RUNNING, FINISHED                           // 1 representa os estados carregando corrigindo...
 }
 
 fun main() {
-    val state = State.RUNNING                         // 2
+    val state = State.RUNNING                         // 2  define um estado e executa o que é a opção igual o switch
     val message = when (state) {                      // 3
         State.IDLE -> "It's idle"
         State.RUNNING -> "It's running"
