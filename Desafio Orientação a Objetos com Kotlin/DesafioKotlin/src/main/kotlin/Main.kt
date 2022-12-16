a53d569356b7bfbb7e08b1e9bc2b5784ab196456
Leandro <leandrogerolim@gmail.com>
2022-12-16 18:53:46 -0400

    Desafio Orientação a Objetos com Kotlin / somente o enum

--- Desafio Orientação a Objetos com Kotlin/DesafioKotlin/src/main/kotlin/Main.kt
+++ Desafio Orientação a Objetos com Kotlin/DesafioKotlin/src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
+
+// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
 class Usuario
@@ -6,39 +9,14 @@ data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    var inscritos = mutableListOf<Usuario> (
+    val inscritos = mutableListOf<Usuario>()
 
-
-
-    )
-
-    fun matricular(usuario: Usuario): String {
-
-
-
-        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(usuario: Usuario) {
+        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
-        fun main() {
-
-
-            val nivel = Nivel.DIFICIL
-            val message = when (nivel) {
-                Nivel.DIFICIL -> "DIFICIL"
-                Nivel.BASICO -> "DIFICIL"
-
-                Nivel.INTERMEDIARIO -> "DIFICIL"
-            }
-            println(message)
-        }
-
-
-
-
-
-
-
-
-    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-   // TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+fun main() {
+    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+}
