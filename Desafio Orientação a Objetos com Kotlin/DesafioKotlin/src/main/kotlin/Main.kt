5af8a2bc1cd172dd8b34babbe07a8c760072e7a1
Leandro <leandrogerolim@gmail.com>
2022-12-16 12:04:36 -0400

    Desafio Orientação a Objetos com Kotlin.

--- Desafio Orientação a Objetos com Kotlin/DesafioKotlin/src/main/kotlin/Main.kt
+++ Desafio Orientação a Objetos com Kotlin/DesafioKotlin/src/main/kotlin/Main.kt
@@ -6,14 +6,39 @@ data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
+    var inscritos = mutableListOf<Usuario> (
 
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+
+    )
+
+    fun matricular(usuario: Usuario): String {
+
+
+
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
     }
 }
 
-fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
-}
+        fun main() {
+            val ConteudoEducacional = ConteudoEducacional()Educacional()
+
+
+            val nivel = Nivel.DIFICIL
+            val message = when (nivel) {
+                Nivel.DIFICIL -> "DIFICIL"
+                Nivel.BASICO -> "DIFICIL"
+                Nivel.INTERMEDIARIO -> "DIFICIL"
+            }
+            println(message)
+        }
+
+
+
+
+
+
+
+
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+   // TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
